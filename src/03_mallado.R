ca65491c2de053d82db5bf99d2df728f3bf99cf8
dtorresalvarez <40713854+dtorresalvarez@users.noreply.github.com>
2018-07-13 20:16:47 +0100

    Update 03_mallado.R

--- src/03_mallado.R
+++ src/03_mallado.R
@@ -1,6 +1,6 @@
 ##########################################################################
 # Proyecto: Aire Analytics
-# Fecha última modificación: 2018-07-01
+# Fecha última modificación: 2018-07-13
 # Código: Mallado de la ciudad de Madrid
 ##########################################################################
 library(rgeos)
@@ -9,7 +9,7 @@ library(rgeos)
 # 1. Contorno de la ciudad de Madrid
 #-----------------------------------------------------------
 plot(barrios)
-points(estaciones$LONG.DD,estaciones$LAT.DD,col="red",pch=16)
+points(estaciones_ayuntamiento$LONG.DD,estaciones_ayuntamiento$LAT.DD,col="red",pch=16)
 # Juntamos el mallado con los barrios de Madrid
 madrid_centro = gUnaryUnion(barrios,barrios$dummy)
 # intersección del grid con el mapa de los barrios
@@ -51,7 +51,7 @@ barrios_agg2 <- spTransform(barrios_agg2, CRS("+init=epsg:4326"))
 qpal <- colorBin("Reds", barrios_agg2$ORIG_FID, bins=5)
 plot3 <- leaflet(barrios_agg2) %>%
   addPolygons(stroke = TRUE,opacity = 1,fillOpacity = 0, smoothFactor = 0.5,
-              color="black",fillColor = ~qpal(ORIG_FID),weight = 0.5) #%>%
+              color="black",fillColor = ~qpal(ORIG_FID),weight = 1) #%>%
   #addLegend(values=~ORIG_FID,pal=qpal,title="Niveles NOx de Madrid")
 
 # Mallado de la ciudad de Madrid en el mapa
@@ -69,7 +69,7 @@ plot5
 #mallado con los centroides de cada celda
 trueCentroids = gCentroid(barrios_agg,byid=TRUE)
 plot(barrios_agg)
-points(estaciones$LONG.DD,estaciones$LAT.DD,col="red",pch=16)
+points(estaciones_ayuntamiento$LONG.DD,estaciones_ayuntamiento$LAT.DD,col="red",pch=16)
 points(coordinates(barrios_agg),pch=1,col="blue")
 points(trueCentroids,pch=3, col="blue")
 head(coordinates(trueCentroids))
@@ -78,12 +78,11 @@ head(coordinates(trueCentroids))
 # 4. Comprobamos que el el contorno y el mallado se ajusta a los bordes de la Ciudad
 #-----------------------------------------------------------
 
-leaflet(data = estaciones) %>% addTiles() %>%
-  addPolygons(data = barrios_agg2)%>%
-  addMarkers(~estaciones$LONG.DD, ~estaciones$LAT.DD, icon = greenLeafIcon)
+leaflet(data = estaciones_ayuntamiento) %>% addTiles() %>%
+  addPolygons(data = barrios_agg2, stroke = TRUE, weight = 1, opacity = 0.5, fill = TRUE, fillOpacity = 0.2)%>%
+  addMarkers(~estaciones_ayuntamiento$LONG.DD, ~estaciones_ayuntamiento$LAT.DD, icon = greenLeafIcon)
 
-leaflet(data = estaciones) %>% addTiles() %>%
-  addPolygons(data = barrios_agg)%>%
-  addMarkers(~estaciones$LONG.DD, ~estaciones$LAT.DD, icon = greenLeafIcon)
-
+leaflet(data = estaciones_ayuntamiento) %>% addTiles() %>%
+  addPolygons(data = barrios_agg, stroke = TRUE, weight = 1, opacity = 0.5, fill = TRUE, fillOpacity = 0.2)%>%
+  addMarkers(~estaciones_ayuntamiento$LONG.DD, ~estaciones_ayuntamiento$LAT.DD, icon = greenLeafIcon)
 
